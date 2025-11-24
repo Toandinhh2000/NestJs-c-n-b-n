a2587630f06a00ce43ca1a57de5099630baea9d8
Toan Dinh <taplamtiktok686@gmail.com>
2025-11-24 14:23:39 +0700

    Nestjs can ban

---
+++ my-project/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
+TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '',
+  database: 'testdb',
+  autoLoadEntities: true,
+  synchronize: true,
+})
+
